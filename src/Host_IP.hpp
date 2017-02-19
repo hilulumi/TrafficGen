21a6430cc052a7fee6298e99df990ff9d62b6fb4
hilulumi <evoke33251@gmail.com>
2017-02-19 01:34:46 -0800

    Host_IP.hpp Finished

--- src/Host_IP.hpp
+++ src/Host_IP.hpp
@@ -14,58 +14,65 @@
 
 class Host_IP{
 private:
-	struct sockaddr_in host;
+	struct in_addr ip;
+	unsigned short port;
 public:
 	Host_IP();
 	Host_IP(unsigned long, unsigned short);
 	Host_IP(std::string, unsigned short);
 	Host_IP(std::string);
 	~Host_IP();
-	std::string getaddr();
+	unsigned long getaddr();
 	void setaddr(std::string);
 	unsigned short getport();
 	void setport(unsigned short);
-	void gethost(struct sockaddr_in &);
+	void sethost(std::string);
 };
 
 Host_IP::Host_IP(){
-
+	setport(p);
+	setaddr("192.168.0.11");
 }
 
-Host_IP::Host_IP(unsigned long addr, unsigned short port){
-
+Host_IP::Host_IP(unsigned long addr, unsigned short p){
+	setport(p);
+	ip.s_addr = htonl(addr);
 }
 
-Host_IP::Host_IP(std::string addr, unsigned short port){
-
+Host_IP::Host_IP(std::string addr, unsigned short p){
+	setaddr(addr);
+	setport(p);
 }
 
-Host_IP::Host_IP(std::string addr_port){
-
+Host_IP::Host_IP(std::string h){
+	sethost(h);
 }
 
 Host_IP::~Host_IP(){
 
 }
 
-std::string Host_IP::getaddr(){
-
+unsigned long Host_IP::getaddr(){
+	return ip.s_addr;
 }
 
 void Host_IP::setaddr(std::string addr){
-
+	inet_aton(addr.c_str(), &ip);
 }
 
 unsigned short Host_IP::getport(){
-
+	return port;
 }
 
-void Host_IP::setport(unsigned short port){
-
+void Host_IP::setport(unsigned short p){
+	port = htons(p);
 }
 
-void Host_IP::gethost(struct sockaddr_in &){
+void Host_IP::sethost(std::string h){
+	size_t split = h.find_last_of(':');
 
+	inet_aton(h.substr(0,split).c_str(), &ip);
+	port = htons(atoi(h.substr(split+1).c_str()));
 }
 
 #endif /* HOST_IP_HPP_ */
